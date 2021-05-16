9a292be22f8b13bb304c263e6006f18b1aec9494
Risch <riscajche7@gmail.com>
2021-05-15 21:33:34 -0600

    Creación del método introducción

--- JuegoDeDragones/Program.cs
+++ JuegoDeDragones/Program.cs
@@ -6,7 +6,16 @@ namespace JuegoDeDragones
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+        }
+        
+        static void introduccion()
+        {
+            Console.WriteLine("Estás en una tierra llena de dragones. Frente a tí ");
+            Console.WriteLine("hay dos cuevas. En una de ellas, el dragón es generoso y ");
+            Console.WriteLine("amigable y compartirá su tesoro contigo. El otro dragón ");
+            Console.WriteLine("es codicioso y está hambriento, y te devorará inmediatamente ");
+            Console.WriteLine();
         }
     }
 }
